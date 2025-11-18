b149bc7976068caf1c91fedd640d3c54834e3e82
akariwill <wildanjrbusiness@gmail.com>
2025-11-18 17:22:56 +0700

    Refactor ecosystem.config.js for improved path management and enable autorestart for whatsapp-bot

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,26 @@
 module.exports = {
   apps : [
     {
-      name: 'chatbot',
-      script: 'app.py',
-      interpreter: './venv/bin/python3',
-      cwd: '/chatbot/', 
+      name: "chatbot",
+      script: "app.py",
+      cwd: "/home/noc/chatbot",
+      interpreter: "/home/noc/chatbot/venv/bin/python3",
       watch: false,
       env: {
-        "NODE_ENV": "production",
+        NODE_ENV: "production",
+        OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+        LANGCHAIN_TRACING_V2: "false"
       }
     },
     {
-      name: 'whatsapp-bot',
-      script: 'index.js',
-      cwd: '/chatbot/whatsapp-bot/', 
+      name: "whatsapp-bot",
+      script: "index.js",
+      cwd: "/home/noc/chatbot/whatsapp-bot",
       watch: false,
-      autorestart: false, 
+      autorestart: true, 
       env: {
-        "NODE_ENV": "production",
+        NODE_ENV: "production",
+        CHATBOT_API_URL: "http://160.25.222.84:8001/chat"
       }
     }
   ]
